0d10a9ec3cf957d4ff4c36cd7be6241bfb8db4c5
”AlenaShkr” <elalshkr@gmail.com>
2021-02-28 21:41:31 +0300

    add onPush strategy and method add and delete count of book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { IBookModel } from './models/book-model';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,15 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { IBookModel } from 'src/app/models/book-model';
 
 @Component({
   selector: 'app-cart-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.css'],
 })
 export class CartItemComponent implements OnInit {
   @Input() book: IBookModel;
 
+  count: number = 1;
+  buyIsAvailable: boolean;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.buyIsAvailable = true;
+  }
+
+  Add() {
+    this.count += 1;
+    this.buyIsAvailable = false;
+  }
+
+  Delete() {
+    if (this.count > 1) {
+      this.count -= 1;
+    } if (this.count === 1) {
+      this.buyIsAvailable = true;
+    }
+  }
 }
