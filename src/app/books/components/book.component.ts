8f762a48e3e77a4301b66cf027bf61643ce7223d
”AlenaShkr” <elalshkr@gmail.com>
2021-03-03 22:26:59 +0300

    add logic of bookservice

--- src/app/books/components/book.component.ts
+++ src/app/books/components/book.component.ts
@@ -1,33 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-import { Category, IBookModel } from '../models/book-model';
+import { IBookModel } from '../models/book-model';
+import { BooksService } from '../services/books.service';
 
-const listOfBooks: IBookModel[] = [
-  {
-    name: 'The Little Prince',
-    description: 'shdklhkjhjgsrhjkahjkrhfjh',
-    price: 13.99,
-    category: Category.novella,
-    createDate: 'April 1943 ',
-    isAvailable: true,
-  },
-  {
-    name: 'The Little',
-    description: 'shdklhkjhjgsrhjkahjkrhfjh',
-    price: 10.99,
-    category: Category.poem,
-    createDate: 'April 1943 ',
-    isAvailable: false,
-  },
-  {
-    name: 'We',
-    description: 'shdklhkjhjgsrh jkahjkrhfjhfdkjk jkhjggfjdvbb fjfbvbbvbvVmfkl jsaajhahfhjdhdjsbhbjhb bd hbb hfbdhbb hfbhd',
-    price: 10.99,
-    category: Category.poem,
-    createDate: 'May 1987 ',
-    isAvailable: true,
-  },
-];
 
 @Component({
   selector: 'app-book',
@@ -37,11 +12,13 @@ const listOfBooks: IBookModel[] = [
 export class BookComponent implements OnInit {
   @Output() byeRequestEvent = new EventEmitter<IBookModel>();
 
-  books = listOfBooks;
+  books: IBookModel[];
+  
+  constructor(private booksService: BooksService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.books = this.booksService.getBooks();
+  }
 
   onBye(book: IBookModel): void {
     this.byeRequestEvent.emit(book);
--- src/app/books/services/books.service.ts
+++ src/app/books/services/books.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
 
+import { Category, IBookModel } from '../models/book-model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
+  listOfBooks: IBookModel[] = [
+    {
+      name: 'The Little Prince',
+      description: 'shdklhkjhjgsrhjkahjkrhfjh',
+      price: 13.99,
+      category: Category.novella,
+      createDate: 'April 1943 ',
+      isAvailable: true,
+    },
+    {
+      name: 'The Little',
+      description: 'shdklhkjhjgsrhjkahjkrhfjh',
+      price: 10.99,
+      category: Category.poem,
+      createDate: 'April 1943 ',
+      isAvailable: false,
+    },
+    {
+      name: 'We',
+      description: 'shdklhkjhjgsrh jkahjkrhfjhfdkjk jkhjggfjdvbb fjfbvbbvbvVmfkl jsaajhahfhjdhdjsbhbjhb bd hbb hfbdhbb hfbhd',
+      price: 10.99,
+      category: Category.poem,
+      createDate: 'May 1987 ',
+      isAvailable: true,
+    },
+  ];
+  
 
   constructor() { }
+
+  getBooks() {
+    return this.listOfBooks;
+  }
 }
